cd75a698488969dc9e3588b7cf0b6e99ce7f93d0
jesus <jesus@jesus-PROX15>
2022-09-29 12:57:15 +0200

    ahora funciona el test

--- src/Ejercicios/Test/test2.java
+++ src/Ejercicios/Test/test2.java
@@ -3,10 +3,12 @@ package Ejercicios.Test;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class test2 {
 	public static void testRecFinal() {
 		String filePath = "ficheros/alumnos/PI1Ej2DatosEntrada.txt";
+		
 		Consumer<String> cnsmr = x -> {
 			 String[] datos =  x.split(",");
 			 Integer a  = Integer.parseInt(datos[0]);
@@ -15,11 +17,10 @@ public class test2 {
 			 System.out.println("Cadena: " + s);
 			 System.out.println("Versión recursiva final" + Ejercicios.E2.Ej2Rec(a, b, s));
 		};
-		Consumer<String> e  = x ->{
-			System.out.println("mecachis");
-		};
+	
 		try {
-			Files.lines(Paths.get(filePath)).forEach(e);
+			Stream<String> lineas = Files.lines(Paths.get(filePath));
+			lineas.forEach(cnsmr);
 		} catch (Exception err) {
 			err.printStackTrace();
 		}
