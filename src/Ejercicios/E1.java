2c9e7217a9c0d2f8c2f354b9b808c122b60bf11d
jesus <jesus@jesus-PROX15>
2022-10-03 11:32:58 +0200

    epico

--- src/Ejercicios/E1.java
+++ src/Ejercicios/E1.java
@@ -14,7 +14,7 @@ public class E1 {
 		// creamos el diccionario 
 		Map<Integer, List<String>> d = new HashMap<>();
 		//  creamos la semilla 
-		 EnteroCadena s = new EnteroCadena(varA, varB);
+		EnteroCadena s = new EnteroCadena(varA, varB);
 		// creamos el predicado 
 		Predicate<EnteroCadena> prdct = x->{
 			return x.a()< varC;
@@ -24,16 +24,16 @@ public class E1 {
 		// bueno mejor no
 		Function<EnteroCadena, EnteroCadena>  funk = x-> {
 			return new EnteroCadena(x.a(), x.a()%3==0?x.s()+x.a().toString():x.s().substring(x.a()%x.s().length()));
-			
+
 		};
-//		creamos el bucle iterativo
+		//		creamos el bucle iterativo
 		while (prdct.test(s)) {
-	String valor = s.s()+varD;
-	Integer clave = valor.length();
+			String valor = s.s()+varD;
+			Integer clave = valor.length();
 			if(clave<varE){if(d.containsKey(clave))  { // existe ya la clave
 				List<String>ls = d.get(clave);
-			ls.add(valor);
-			d.put(clave, ls);
+				ls.add(valor);
+				d.put(clave, ls);
 			}else {
 				// no existe la clave 
 				List<String> ls = new LinkedList<>();
@@ -41,25 +41,25 @@ public class E1 {
 				d.put(clave, ls);
 			}
 			}
-			
+
 		}
-		
-	return d; 	
+
+		return d; 	
 	}
-	
+
 	// versión recirsiva final
 	public static  Map<Integer, List<String>> ej1Recursivo ( Integer varA, String varB,
 			Integer varC,String varD, Integer varE){
-		 EnteroCadena s = new EnteroCadena(varA, varB);
-		 Map<Integer, List<String>> d = new HashMap<>();
-			Function<EnteroCadena, EnteroCadena>  funk = x-> {
-				return new EnteroCadena(x.a(), x.a()%3==0?x.s()+x.a().toString():x.s().substring(x.a()%x.s().length()));
-				
-			};
-		
-				
-				return null;
-		
+		EnteroCadena s = new EnteroCadena(varA, varB);
+		Map<Integer, List<String>> d = new HashMap<>();
+		Function<EnteroCadena, EnteroCadena>  funk = x-> {
+			return new EnteroCadena(x.a(), x.a()%3==0?x.s()+x.a().toString():x.s().substring(x.a()%x.s().length()));
+
+		};
+
+
+		return d;
+
 	}
 	public static  Map<Integer, List<String>> ej1RecursivoAux ( Integer varA, String varB,
 			Integer varC,String varD, Integer varE,  Map<Integer, List<String>> d, EnteroCadena s,Function<EnteroCadena, EnteroCadena> funk){
@@ -68,10 +68,10 @@ public class E1 {
 		if(clave<varE) {
 			if(d.containsKey(clave))  { // existe ya la clave
 				List<String>ls = d.get(clave);
-			ls.add(valor);
-			d.put(clave, ls);
-			EnteroCadena ns = funk.apply(s);
-			ej1RecursivoAux(varA, varB, varC, varD, varE, d, ns, funk);
+				ls.add(valor);
+				d.put(clave, ls);
+				EnteroCadena ns = funk.apply(s);
+				ej1RecursivoAux(varA, varB, varC, varD, varE, d, ns, funk);
 			}else {
 				// no existe la clave 
 				List<String> ls = new LinkedList<>();
@@ -80,13 +80,13 @@ public class E1 {
 				EnteroCadena ns = funk.apply(s);
 				ej1RecursivoAux(varA, varB, varC, varD, varE, d, ns, funk);
 			}
-			
+
 		}
 		return d;
-		
+
 	}
-	
-	
-	
+
+
+
 
 }
